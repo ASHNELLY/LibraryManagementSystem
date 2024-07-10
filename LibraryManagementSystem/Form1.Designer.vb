72532425c53e1d3311675bbea2e6dc10c164ec38
HP <HP@VENOMIZER>
2024-07-10 18:42:17 +0300

    Add project files.

---
+++ LibraryManagementSystem/Form1.Designer.vb
@@ -0,0 +1,235 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.lblAuthor = New System.Windows.Forms.Label()
+        Me.lblYearPublished = New System.Windows.Forms.Label()
+        Me.lblGenre = New System.Windows.Forms.Label()
+        Me.txtTitle = New System.Windows.Forms.TextBox()
+        Me.txtAuthor = New System.Windows.Forms.TextBox()
+        Me.txtYearPublished = New System.Windows.Forms.TextBox()
+        Me.txtGenre = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.dgvBooks = New System.Windows.Forms.DataGridView()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        CType(Me.dgvBooks, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(40, 19)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(109, 37)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Text = "TITLE"
+        '
+        'lblAuthor
+        '
+        Me.lblAuthor.AutoSize = True
+        Me.lblAuthor.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAuthor.Location = New System.Drawing.Point(20, 83)
+        Me.lblAuthor.Name = "lblAuthor"
+        Me.lblAuthor.Size = New System.Drawing.Size(158, 37)
+        Me.lblAuthor.TabIndex = 1
+        Me.lblAuthor.Text = "AUTHOR"
+        '
+        'lblYearPublished
+        '
+        Me.lblYearPublished.AutoSize = True
+        Me.lblYearPublished.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblYearPublished.Location = New System.Drawing.Point(10, 176)
+        Me.lblYearPublished.Name = "lblYearPublished"
+        Me.lblYearPublished.Size = New System.Drawing.Size(306, 37)
+        Me.lblYearPublished.TabIndex = 2
+        Me.lblYearPublished.Text = "YEAR PUBLISHED"
+        '
+        'lblGenre
+        '
+        Me.lblGenre.AutoSize = True
+        Me.lblGenre.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblGenre.Location = New System.Drawing.Point(20, 289)
+        Me.lblGenre.Name = "lblGenre"
+        Me.lblGenre.Size = New System.Drawing.Size(135, 37)
+        Me.lblGenre.TabIndex = 3
+        Me.lblGenre.Text = "GENRE"
+        '
+        'txtTitle
+        '
+        Me.txtTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTitle.Location = New System.Drawing.Point(360, 12)
+        Me.txtTitle.Name = "txtTitle"
+        Me.txtTitle.Size = New System.Drawing.Size(194, 44)
+        Me.txtTitle.TabIndex = 4
+        '
+        'txtAuthor
+        '
+        Me.txtAuthor.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtAuthor.Location = New System.Drawing.Point(360, 83)
+        Me.txtAuthor.Name = "txtAuthor"
+        Me.txtAuthor.Size = New System.Drawing.Size(194, 44)
+        Me.txtAuthor.TabIndex = 5
+        '
+        'txtYearPublished
+        '
+        Me.txtYearPublished.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtYearPublished.Location = New System.Drawing.Point(360, 176)
+        Me.txtYearPublished.Name = "txtYearPublished"
+        Me.txtYearPublished.Size = New System.Drawing.Size(194, 44)
+        Me.txtYearPublished.TabIndex = 6
+        '
+        'txtGenre
+        '
+        Me.txtGenre.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtGenre.Location = New System.Drawing.Point(360, 289)
+        Me.txtGenre.Name = "txtGenre"
+        Me.txtGenre.Size = New System.Drawing.Size(194, 44)
+        Me.txtGenre.TabIndex = 7
+        '
+        'btnAdd
+        '
+        Me.btnAdd.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.btnAdd.ForeColor = System.Drawing.Color.Black
+        Me.btnAdd.Location = New System.Drawing.Point(0, 0)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(179, 69)
+        Me.btnAdd.TabIndex = 10
+        Me.btnAdd.Text = "ADD"
+        Me.btnAdd.UseVisualStyleBackColor = False
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnUpdate.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.btnUpdate.ForeColor = System.Drawing.Color.Black
+        Me.btnUpdate.Location = New System.Drawing.Point(209, -1)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(184, 70)
+        Me.btnUpdate.TabIndex = 9
+        Me.btnUpdate.Text = "UPDATE"
+        Me.btnUpdate.UseVisualStyleBackColor = False
+        '
+        'btnDelete
+        '
+        Me.btnDelete.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.btnDelete.ForeColor = System.Drawing.Color.Black
+        Me.btnDelete.Location = New System.Drawing.Point(206, 114)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(184, 64)
+        Me.btnDelete.TabIndex = 10
+        Me.btnDelete.Text = "DELETE"
+        Me.btnDelete.UseVisualStyleBackColor = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.btnClear.ForeColor = System.Drawing.Color.Black
+        Me.btnClear.Location = New System.Drawing.Point(3, 114)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(179, 64)
+        Me.btnClear.TabIndex = 11
+        Me.btnClear.Text = "CLEAR"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'dgvBooks
+        '
+        Me.dgvBooks.BackgroundColor = System.Drawing.SystemColors.ControlLight
+        Me.dgvBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvBooks.Location = New System.Drawing.Point(595, 12)
+        Me.dgvBooks.Name = "dgvBooks"
+        Me.dgvBooks.RowHeadersWidth = 82
+        Me.dgvBooks.RowTemplate.Height = 33
+        Me.dgvBooks.Size = New System.Drawing.Size(457, 689)
+        Me.dgvBooks.TabIndex = 12
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.White
+        Me.Panel1.Controls.Add(Me.Panel2)
+        Me.Panel1.Location = New System.Drawing.Point(17, 467)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(472, 234)
+        Me.Panel1.TabIndex = 13
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.GhostWhite
+        Me.Panel2.Controls.Add(Me.btnUpdate)
+        Me.Panel2.Controls.Add(Me.btnDelete)
+        Me.Panel2.Controls.Add(Me.btnAdd)
+        Me.Panel2.Controls.Add(Me.btnClear)
+        Me.Panel2.Location = New System.Drawing.Point(30, 23)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(393, 198)
+        Me.Panel2.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(1064, 821)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.dgvBooks)
+        Me.Controls.Add(Me.txtGenre)
+        Me.Controls.Add(Me.txtYearPublished)
+        Me.Controls.Add(Me.txtAuthor)
+        Me.Controls.Add(Me.txtTitle)
+        Me.Controls.Add(Me.lblGenre)
+        Me.Controls.Add(Me.lblYearPublished)
+        Me.Controls.Add(Me.lblAuthor)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgvBooks, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents lblAuthor As Label
+    Friend WithEvents lblYearPublished As Label
+    Friend WithEvents lblGenre As Label
+    Friend WithEvents txtTitle As TextBox
+    Friend WithEvents txtAuthor As TextBox
+    Friend WithEvents txtYearPublished As TextBox
+    Friend WithEvents txtGenre As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents dgvBooks As DataGridView
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+End Class
